b49ca0d8ad1dbc29217a713331577247406ae00f
aademilu <31484193+aademilu@users.noreply.github.com>
2022-06-05 18:52:19 -0400

    remove uneeded try/except

--- runGTFConvert.py
+++ runGTFConvert.py
@@ -27,59 +27,30 @@ def main(args):
     #Blast
     if csv:
         for i in csv:
-            try:
                 os.system('python {} -i {} -t csv'.format(blast, i))
-            except:
-                print(f'File {i} Not Compatible')
     if tsv:
         for i in tsv:
-            try:
                 os.system('python {} -i {} -t tsv'.format(blast, i))
-            except:
-                print(f'File {i} Not Compatible')
     if xlsx:
         for i in xlsx:
-            try:
                 os.system('python {} -i {} -t xlsx'.format(blast, i))
-            except:
-                print(f'File {i} Not Compatible')
     if fmt6:
         for i in fmt6:
-            try:
                 os.system('python {} -i {} -t tsv'.format(blast, i))
-            except:
-                print(f'File {i} Not Compatible')
     #CD-HIT
     if clstr:
         for i in clstr:
             if 'cd-hit-2d' in i:
-                try:
                     os.system('python {} -i {} -t 2'.format(cdhit, i))
-                except:
-                    print(f'File {i} Not Compatible')
             else:
-                try:
                     os.system('python {} -i {} -t 1'.format(cdhit, i))
-                except:
-                    print(f'File {i} Not Compatible')
-
     #Exonerate
     if gff2:
         for i in gff2:
-            try:
-                #os.system('python {} -i {} -o {}.gtf'.format(exonerate, i, os.path.splitext(i))[0])
                 os.system('python {} -i {} -o {}.gtf'.format(exonerate, i, os.path.splitext(i)[0]))
-            except:
-                print(f'File {i} Not Compatible')
     if out:
         for i in out:
-            try:
                 os.system('python {} -i {} -o {}.gtf'.format(exonerate, i, os.path.splitext(i)[0]))
-            except:
-                print(f'File {i} Not Compatible')
-    #Output into GTF folder within parent folder
-
-
 
 if __name__ == '__main__':
     parser=argparse.ArgumentParser()
